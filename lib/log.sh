fff322cb9adc159f56f5e8544ae9abdebd565cb7
furudenipa <kazutti.344@gmail.com>
2026-01-24 01:11:58 +0900

    logを追加

---
+++ lib/log.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+info () {
+  printf "\r  [ \033[00;37m..\033[0m ] %s\n" "$1"
+}
+
+user () {
+  printf "\r  [ \033[0;34m??\033[0m ] %s\n" "$1"
+}
+
+success () {
+  printf "\r  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+warn () {
+  printf "\r  [\033[0;33mWARN\033[0m] %s\n" "$1"
+}
+
+fail () {
+  printf "\r  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  echo ''
+  exit
+}
