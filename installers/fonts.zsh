41d53431453d4df478d606a1208520e6403139eb
furudenipa <kazutti.344@gmail.com>
2026-02-07 01:49:21 +0900

    fontを追加

--- installers/fonts.zsh
+++ installers/fonts.zsh
@@ -1,8 +1,19 @@
-# fontを入れたい
-# Nerd Fontがいい
-# 参考 https://qiita.com/sl2/items/bf58dadb261c0a019571
-#
-# brew install --cask font-hack-nerd-font
-#
-# brew がinstall済みであることは前提として良い
-# not darwinのときは一旦考えなくても良いかも
+source "$ROOT/lib/log.zsh"
+source "$ROOT/lib/env.zsh"
+
+# Reference blog:
+# https://qiita.com/sl2/items/bf58dadb261c0a019571
+
+_install() {
+  if is_macos; then
+    if brew list --cask font-hack-nerd-font >/dev/null 2>&1; then
+      info "font-hack-nerd-font is already installed."
+    else
+      brew install --cask font-hack-nerd-font
+    fi
+    return 0
+  fi
+
+  fail "supported on macOS only."
+  return 1
+}
